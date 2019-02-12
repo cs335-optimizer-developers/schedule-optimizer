ff1a01d995e190f484f365177512b3a0b3ed69dd
James White <sirjwhite@gmail.com>
2019-02-12 09:27:14 -0600

    Four methods.

---
+++ optimizer/src/display/Display.java
@@ -0,0 +1,25 @@
+package display;
+
+public class Display {
+
+	private void UI1() {
+		
+	}
+	
+	private void UI2() {
+		
+	}
+	
+	private void UI3() {
+			
+		}
+	
+	private void UI4() {
+		
+	}
+	
+	private void UI5() {
+		
+	}
+	
+}
