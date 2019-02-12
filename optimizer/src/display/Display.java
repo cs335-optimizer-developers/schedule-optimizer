a585825d68b7c8dd3d68cfc3c66b942c8d48ddde
James White <sirjwhite@gmail.com>
2019-02-12 09:29:40 -0600

    Four display classes.

--- optimizer/src/display/Display.java
+++ optimizer/src/display/Display.java
@@ -2,24 +2,4 @@ package display;
 
 public class Display {
 
-	private void UI1() {
-		
-	}
-	
-	private void UI2() {
-		
-	}
-	
-	private void UI3() {
-			
-		}
-	
-	private void UI4() {
-		
-	}
-	
-	private void UI5() {
-		
-	}
-	
 }
