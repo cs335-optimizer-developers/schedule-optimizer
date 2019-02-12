b28ac31f5fb53aec112186e3b66596b3b67fa281
Naissa <naissa.charles@my.wheaton.edu>
2019-02-12 11:08:31 -0600

    not done but search and enter button

--- optimizer/src/display/Display5.java
+++ optimizer/src/display/Display5.java
@@ -15,7 +15,7 @@ import java.awt.TextField;
 
 public class Display5 {
 	
-	static JTextField searchField;
+	/*static JTextField searchField;
 	
 	//public TextField (String intitalText);
 	static GraphicsConfiguration gc;
@@ -39,9 +39,50 @@ public class Display5 {
 				frame.getContentPane().add(searchField);
 				frame.setVisible(true);
 				//you need text field and button
+	}*/
+	
+	SearchBar(){
+		
+		JFrame frame = new JFrame("Schedule Optimizer-Search Bar");
+		
+		//enter button
+		JButton button = new JButton("Enter");
+		button.setBounds(100,100,140,40);
+		
+		
+		JLabel label = new JLabel();
+		label.setText("Search");
+		label.setBounds(10,10,100,100);
+		
+		
+		JLabel label1 = new JLabel();
+		label1.setBounds(10,110,200,100);
+		
+		JTextField searchField = new JTextField();
+		searchField.setBounds(110,50,130,30);
+		
+		frame.add(label1);
+		frame.add(searchField);
+		frame.add(label);
+		frame.add(button);
+		frame.setSize(400, 400);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		button.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0) {
+				label1.setText("YOU SEARCHED!");
+			}
+		});
+		
+		
+		public static void main(String[] args) {
+			new SearchBar();
+		}
+		
 	}
-	
-	
 	
 	
 	
