7534766fc0764562da64bd764970e64dcd8f27d2
nyameye <nyameye@10.12.254.80>
2019-02-13 00:12:53 -0600

    final mockup

--- optimizer/src/display/Display3.java
+++ optimizer/src/display/Display3.java
@@ -4,25 +4,21 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class Display3{
-	
 	public static void createWindow() {
 		//Create and set up the window.
         JFrame frame = new JFrame("Schedule Optimizer");
-        
         //creating elements
         JButton button = new JButton("Enter");
         JLabel label = new JLabel();
         JTextField major1 = new JTextField("Enter First Major");
         JTextField major2 = new JTextField("Enter Second Major");
         JTextField major3 = new JTextField("Enter Minor");
-        
         //positioning the elements
         button.setBounds(300, 100, 90, 20);
         major1.setBounds(50, 50, 150, 50);
         major2.setBounds(250, 50, 150, 50);
         major3.setBounds(450, 50, 150, 50);
-        label.setBounds(300, 150, 100,200);
-       
+        label.setBounds(300, 50, 200, 400);
         //adding elements to the screen
         frame.add(label);
         frame.add(button);
@@ -35,23 +31,15 @@ public class Display3{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         button.addActionListener(new ActionListener() {
-        	
         		public void actionPerformed(ActionEvent e) {
         			label.setText("MAJORS ENTERED");
         		}
-        	
         });
 
 	}
 
 	public static void main(String argv[]) {
-		/*javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createWindow();
-            }
-        });*/
 		createWindow();
-		
 	}
 	
 }
