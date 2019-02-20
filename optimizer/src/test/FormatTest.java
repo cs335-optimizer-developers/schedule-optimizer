da7bc7b121dd4e81941f93e010f785f62c035213
James White <jameswhite@cslab18.wheaton.edu>
2019-02-20 15:43:49 -0600

    Fixed Test Error.

--- optimizer/src/test/FormatTest.java
+++ optimizer/src/test/FormatTest.java
@@ -1,14 +1,6 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 class FormatTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
