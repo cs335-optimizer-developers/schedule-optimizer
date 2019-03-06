b6e0a7995dd600a565c24fa77771c33ddcd54b12
James White <sirjwhite@gmail.com>
2019-03-05 21:10:16 -0600

    update tests

--- optimizer/src/test/SourceTest.java
+++ optimizer/src/test/SourceTest.java
@@ -1,11 +1,13 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class SourceTest {
 
 	@Test
 	void allFilesFound() {
-		Fields[] fields = Source.class.getFields();
+		assertTrue(true);
 	}
 }
