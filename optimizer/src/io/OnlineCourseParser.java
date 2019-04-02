e334d793a119b02adf0bc5814c48be28ea6487de
James White <sirjwhite@gmail.com>
2019-04-02 10:07:13 -0500

    Removed warning.

--- optimizer/src/io/OnlineCourseParser.java
+++ optimizer/src/io/OnlineCourseParser.java
@@ -6,80 +6,81 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 
-
+/**
+ * 
+ * Class to read in information from Wheaton's catalog, to generate
+ * .cur and .det files.
+ * 
+ * This file is not used at runtime, only to generate files that will
+ * be read in at runtime.
+ * 
+ * @author Naissa Charles
+ *
+ */
 public class OnlineCourseParser {
 
-//import java.io.IOException;
-
-
-public static class CourseParser {
-	/*throw exception (declaration)*/
 	public static void main(String[] args) throws IOException {
 		
 		Document courseAZ = null;
 		
+		courseAZ = Jsoup.connect("https://catalog.wheaton.edu/azindex/").get();
+			
+			
+		String titles = courseAZ.title();
+			
+		System.out.println ("Title : " + titles );
 		
-			courseAZ = Jsoup.connect("https://catalog.wheaton.edu/azindex/").get();
-				
-				
-			String titles = courseAZ.title();
-				
-			System.out.println ("Title : " + titles );
+		Elements links = courseAZ.select("a[href]");
+		
+		for (Element link: links) {
+			//method call to courseDescrip. Will look like courseDescrip(links.attr("href").toString());
+			courseDescrip(links.attr("href").toString());
+						//maybe make a links.attr("href").toString() variable to keep clean
 			
-			Elements links = courseAZ.select("a[href]");
-			
-			for (Element link: links) {
-				//method call to courseDescrip. Will look like courseDescrip(links.attr("href").toString());
-				courseDescrip(links.attr("href").toString());
-							//maybe make a links.attr("href").toString() variable to keep clean
-				
-				System.out.println("\nlink : " + link.attr("href"));
-				System.out.println("Text : " + link.text());
-			}
-			
-			//Document example = null;
+			System.out.println("\nlink : " + link.attr("href"));
+			System.out.println("Text : " + link.text());
+		}
+		
+		//Document example = null;
 
-			//example = Jsoup.connect(links.attr("href").toString()).get();
-		}
+		//example = Jsoup.connect(links.attr("href").toString()).get();
+	}
 	
 	
-		public static void courseDescrip (String info) {
+	public static void courseDescrip (String info) {
+		
+		Document doc = null;
+		
+		try {
+			doc = Jsoup.connect(info).get();
 			
-			Document doc = null;
+			String title = doc.title();
 			
-			try {
-				doc = Jsoup.connect(info).get();
+			System.out.println ("Title : " + title );
+			
+						
+			String html = "<div <p class = courseblocktitle noindent> <strong></strong> </p> ";
+			
+			Document docs = Jsoup.parseBodyFragment(html);
+			
+			@SuppressWarnings("unused")
+			Element body = docs.body(); 
+			
+			Elements paragraphs = doc.getElementsByClass("courseblockdesc noindent");
+			for (Element paragraph : paragraphs ) {
+				System.out.println("\n descriptions: " + paragraph.text());
 				
-				String title = doc.title();
-				
-				System.out.println ("Title : " + title );
-				
-							
-				String html = "<div <p class = courseblocktitle noindent> <strong></strong> </p> ";
-				
-				Document docs = Jsoup.parseBodyFragment(html);
-				
-				Element body = docs.body(); 
-				
-				Elements paragraphs = doc.getElementsByClass("courseblockdesc noindent");
-				for (Element paragraph : paragraphs ) {
-					System.out.println("\n descriptions: " + paragraph.text());
-					
-				}
-				
-				Elements paragraphs1 = doc.getElementsByClass("courseblocktitle noindent");
-				for (Element paragraph : paragraphs1 ) {
-					System.out.println("\n course name,number,and credits: " + paragraph.text());
-					 
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-				
+			
+			Elements paragraphs1 = doc.getElementsByClass("courseblocktitle noindent");
+			for (Element paragraph : paragraphs1 ) {
+				System.out.println("\n course name,number,and credits: " + paragraph.text());
+				 
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+			
 	}
 }
-
-
-		
